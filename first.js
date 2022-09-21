485e7d6431c4045e8c1b42a3774ed85e37ca1e93
Ankit-Gahukar <topdacankit96@gmail.com>
2022-09-21 14:11:42 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,12 @@
+var a = 64;
+
+var square = a * a;
+var root = Math.sqrt(a);
+console.log(square);
+console.log(root);
+
+function myFunction(a, b) {
+  return a * b;
+}
+
+console.log(myFunction(4, 3));
