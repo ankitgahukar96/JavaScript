d3fc6704d37561b6a12637bcc0391f00fd9cde2f
Ankit-Gahukar <topdacankit96@gmail.com>
2022-09-21 14:18:11 +0530

    changes

--- first.js
+++ first.js
@@ -1,9 +1,11 @@
 var a = 64;
 
 var square = a * a;
+var sq = Math.pow(a, 2);
 var root = Math.sqrt(a);
 console.log(square);
 console.log(root);
+console.log(sq);
 
 function myFunction(a, b) {
   return a * b;
